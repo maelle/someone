eb9729e445f1c3878f657b45fb925af8d505e30d
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-02-18 17:12:00 +0100

    adds data

---
+++ R/sa_names.R
@@ -0,0 +1,9 @@
+#' @title  Names given to babies born i 2016 in South Africa
+#'
+#' @name sa_names
+#' @docType data
+#' @description data about live births collected by Stats SA, see \url{http://www.statssa.gov.za/?page_id=1854&PPN=P0305&SCH=7092}.
+#' The data is distributed with a CC-BY SA license.
+#' @format a data.frame with columns first_name number, sex.
+#' @source Stats SA
+NULL
